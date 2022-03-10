7c5c1b8158a16e5ad6b52c3c3b683566d528800d
brajendrak00068 <brajendra@Brajendras-MacBook-Air-M1.local>
2022-03-10 18:36:20 +0530

    grid spacing

--- src/hooks/useWindowResize.js
+++ src/hooks/useWindowResize.js
@@ -1,39 +1,60 @@
-
-   
 import React, {useEffect, useLayoutEffect, useState} from 'react';
 import {useSelector} from "react-redux";
-import {ENTER_FULL_SCREEN_MODE, EXIT_FULL_SCREEN_MODE} from "../constants";
+import {ENTER_FULL_SCREEN_MODE, EXIT_FULL_SCREEN_MODE, GRID, SPEAKER} from "../constants";
+import { useDocumentSize } from './useDocumentSize';
 
 export function useWindowResize() {
     const layout = useSelector(state => state.layout);
+    const { documentWidth, documentHeight } = useDocumentSize();
     const [windowSize, setWindowSize] = useState({viewportWidth: undefined, viewportHeight: undefined});
 
-    function getDimensions(mode) {
-        let documentWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-        let documentHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    function getDimensions() {
         let viewportHeight, viewportWidth;
-        
-        if (documentHeight > documentWidth*9/16) {
-            viewportWidth = documentWidth - (layout.mode === ENTER_FULL_SCREEN_MODE ||  documentWidth < 1025 ? 0 : 250);
-            viewportHeight = viewportWidth*9/16;
-        } else {
-            viewportHeight = documentHeight- (layout.mode === ENTER_FULL_SCREEN_MODE ? 64 : 128) 
-            viewportWidth = viewportHeight*16/9;
+
+        if ( layout.type === GRID && layout.mode === ENTER_FULL_SCREEN_MODE  ) {
+            return { viewportWidth: (documentHeight -  84) * 16/9, viewportHeight: documentHeight -  84 }
         }
-        return {viewportWidth, viewportHeight};
+        
+        if ( layout.type === GRID ) {
+            return { viewportWidth: (documentHeight -  128) * 16/9, viewportHeight: documentHeight -  128 }
+        }
+
+        if (layout.mode === ENTER_FULL_SCREEN_MODE ) {
+            viewportHeight = documentHeight - 84;
+            viewportWidth = viewportHeight * 16/9;
+            return {viewportWidth , viewportHeight};
+        }
+
+        viewportWidth = documentWidth - documentWidth*20/100;
+        viewportHeight = viewportWidth * 9/16;
+        
+        if (viewportHeight > documentHeight - 128) {
+            viewportHeight = documentHeight - 128
+            viewportWidth  = viewportHeight*16/9
+        }
+
+        return { viewportWidth, viewportHeight };
     }
 
     useEffect(() => {
-        if (layout.mode === EXIT_FULL_SCREEN_MODE ) {
-            window.removeEventListener("resize", handleResize)
-        } else {
-            window.addEventListener("resize", handleResize)
-        }
-        setWindowSize(getDimensions(layout.mode));
+        // if (layout.mode === EXIT_FULL_SCREEN_MODE ) {
+        //     window.removeEventListener("resize", handleResize)
+        // } else {
+        //     window.addEventListener("resize", handleResize)
+        // }
+        setWindowSize(getDimensions());
     }, [layout.mode]);
 
+    useEffect(() => {
+        setWindowSize(getDimensions());
+    }, [layout.type]);
+
+    useEffect(() => {
+        setWindowSize(getDimensions());
+    }, [documentWidth, documentHeight]);
+
     function handleResize() {
-        setWindowSize(getDimensions(layout.mode));
+        setWindowSize(getDimensions());
     }
 
     useEffect(() => {

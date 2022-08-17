6859b546b087b5c4417bab84792e35cbbe6e4707
gurudeep88 <gurudeep88@gmail.com>
2022-08-17 18:11:14 +0530

    api keys changed

--- src/components/meeting/ActionButtons/index.js
+++ src/components/meeting/ActionButtons/index.js
@@ -19,6 +19,7 @@ import MicOffIcon from "@material-ui/icons/MicOff";
 import VideocamIcon from "@material-ui/icons/Videocam";
 import VideocamOffIcon from "@material-ui/icons/VideocamOff";
 import ScreenShareIcon from "@material-ui/icons/ScreenShare";
+import StopScreenShareIcon from '@material-ui/icons/StopScreenShare';
 import PanToolIcon from "@material-ui/icons/PanTool";
 import GroupIcon from "@material-ui/icons/Group";
 import ChatIcon from "@material-ui/icons/Chat";
@@ -54,6 +55,7 @@ import {
   exitFullscreen,
   formatAMPM,
   isFullscreen,
+  isPortrait,
   requestFullscreen,
 } from "../../../utils";
 import classNames from "classnames";
@@ -701,7 +703,7 @@ const ActionButtons = ({ dominantSpeakerId }) => {
     dispatch(clearAllReducers());
     history.push("/leave");
   };
-
+  
   return (
     <Box id="footer" className={classes.root}>
       <Hidden mdDown>
@@ -747,26 +749,11 @@ const ActionButtons = ({ dominantSpeakerId }) => {
         </StyledTooltip>
         <StyledTooltip title={presenting ? "Stop Presenting" : "Share Screen"}>
           {presenting ? (
-            <span
-              className={classnames(
-                "material-icons material-icons-outlined",
-                classes.active,
-                classes.screenShare
-              )}
-              onClick={stopPresenting}
-            >
-              stop_screen_share
-            </span>
+            <StopScreenShareIcon className={classnames(classes.active, classes.screenShare)}
+            onClick={stopPresenting} />
           ) : (
-            <span
-              className={classnames(
-                "material-icons material-icons-outlined",
-                classes.screenShare
-              )}
-              onClick={shareScreen}
-            >
-              screen_share
-            </span>
+            <ScreenShareIcon className={ classes.screenShare}
+             onClick={shareScreen} />
           )}
         </StyledTooltip>
         <StyledTooltip title={raiseHand ? "Hand Down" : "Raise Hand"}>

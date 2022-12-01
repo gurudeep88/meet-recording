445391207c1535e395584d1dfdbb9401f3e9e4c3
brajendrak00068 <brajendra@Brajendras-MacBook-Air-M1.local>
2022-12-02 03:51:46 +0530

    image invert

--- src/components/shared/Video/index.js
+++ src/components/shared/Video/index.js
@@ -4,7 +4,6 @@ import { color } from '../../../assets/styles/_color';
 
 const useStyles = makeStyles(() => ({
     video: {
-        transform: "scaleX(-1)!important",
     }
 }))
 
@@ -22,7 +21,6 @@ const Video = props => {
 
     return (<video playsInline="1" 
                    autoPlay='1' 
-                   className={ !isPresenter && classes.video } 
                    ref={videoElementRef}
                    style={{left: "-1px",top: "-1px",  position: props.position || "absolute", width: "calc(100% + 2px)", height: props.height || "calc(100% + 2px)", objectFit: 'contain', borderRadius: '8px' }}/>);
 }

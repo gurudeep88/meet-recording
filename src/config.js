ebe613c36ffff899e92627b22399f57de7f74232
gurudeep88 <gurudeep88@gmail.com>
2023-07-31 10:09:43 +0530

    env and config sdjuted

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const SARISKA_MEET_APP_API_KEY = process.env.REACT_APP_SARISKA_MEET_APP_API_KEY;
+
+export const API_SERVICE_HOST = 'https://api.sariska.io'
+export const API_SERVICE_HOST_NAME = 'meet.sariska.io'
+export const SARISKA_LOGO = 'https://assets.sariska.io/Logo_Sariska.svg'
+export const DOCUMENT_TITLE='Sariska Meet App'
